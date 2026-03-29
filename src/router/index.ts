dadd7fe39f884cd40076c1f4631511ca8f0ad9ae
eveglowluna <yy15502497613@outlook.com>
2026-03-29 23:38:31 +0800

    fix:路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,18 +8,26 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { title: '主页' },
     },
     {
       path: '/download',
       name: 'download',
       component: () => import('../views/DownloadView.vue'),
+      meta: { title: '下载' },
     },
     {
       path: '/docs',
       name: 'docs',
       component: () => import('../views/DocsView.vue'),
+      meta: { title: '文档' },
     },
   ],
+})
+
+router.afterEach((to) => {
+  const pageTitle = to.meta.title as string | undefined
+  document.title = pageTitle ? `${pageTitle} | ElvaraOS` : 'ElvaraOS'
 })
 
 export default router
